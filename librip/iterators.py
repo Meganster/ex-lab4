f57045b05324c5848653a8bcded0f8082adacba8
Meganster <ppro100takk@mail.ru>
2017-10-27 14:27:52 +0300

    Update itter

--- librip/iterators.py
+++ librip/iterators.py
@@ -1,6 +1,10 @@
 # Итератор для удаления дубликатов
 class Unique(object):
+    # будем хранить в этой переменной
+    # все полученные объекты items
     _lst = []
+    # будем хранить уникальные объекты
+    # в этой переменной
     _new_lst = []
     _index = -1
 
@@ -13,26 +17,29 @@ class Unique(object):
         # По-умолчанию ignore_case = False
         assert len(items) > 0
         self._lst = list(items)
+
         if ignore_case & isinstance(self._lst[0], str):
-            self._lst.sort(key=lambda st: st.lower())
+            self._lst.sort(key = lambda st: st.lower())
         else:
             self._lst.sort()
+
         self._new_lst = []
-        cur_in = 0
+        current_index = 0
         self._new_lst.append(self._lst[0])
+
         for x in self._lst:
             if(ignore_case & isinstance(self._lst[0], str)):
-                if(x.lower() != self._new_lst[cur_in].lower()):
+                if(x.lower() != self._new_lst[current_index].lower()):
                     self._new_lst.append(x)
-                    cur_in += 1
+                    current_index += 1
             else:
-                if(x != self._new_lst[cur_in]):
+                if(x != self._new_lst[current_index]):
                     self._new_lst.append(x)
-                    cur_in += 1
+                    current_index += 1
         self._index = -1
 
     def __next__(self):
-        if self._index >= len(self._new_lst)-1:
+        if self._index >= len(self._new_lst) - 1:
             raise StopIteration
         self._index += 1
         return self._new_lst[self._index];
